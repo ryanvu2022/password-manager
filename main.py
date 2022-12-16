0f4a275fa260821dfade8f77513cd4c7fcb30f13
Thinh <ryanvu2022@gmail.com>
2022-12-16 12:40:46 -0500

    refactor codes

--- main.py
+++ main.py
@@ -51,29 +51,37 @@ locker_img = PhotoImage(file="lock.png")
 canvas.create_image(96, 66, image=locker_img)
 canvas.grid(row=0, column=1)
 
+# Labels
 website_label = Label(text="Website:")
 website_label.config(pady=5)
 website_label.grid(row=1, column=0)
-website_entry = Entry(width=45)
-website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2)
 
 email_label = Label(text="Email/Username:")
 email_label.config(pady=5)
 email_label.grid(row=2, column=0)
-email_entry = Entry(width=45)
-email_entry.insert(0, "ryanvu2022@gmail.com")
-email_entry.grid(row=2, column=1, columnspan=2)
 
 password_label = Label(text="Password:")
 password_label.config(pady=5)
 password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=45)
+website_entry.focus()
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_entry = Entry(width=45)
+email_entry.insert(0, "ryanvu2022@gmail.com")
+email_entry.grid(row=2, column=1, columnspan=2)
+
 password_entry = Entry(width=45)
 password_entry.grid(row=3, column=1, columnspan=2)
 
+# Buttons
 password_button = Button(text="Generate Password", command=generate_password)
 password_button.grid(row=3, column=2)
+
 add_button = Button(text="Add", width=38, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2)
+
 
 window.mainloop()
