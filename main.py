77491caaa31497d918fecb8f84958c7f15d86d1d
Thinh <ryanvu2022@gmail.com>
2022-12-16 15:06:54 -0500

    save data to json file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 def generate_password():
@@ -26,20 +27,30 @@ def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                              f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("password_data.txt", "a") as file:
-                file.write(f"{website} | {email} | {password}\n")
-            website_entry.delete(0, END)
-            email_entry.delete(0, END)
-            email_entry.insert(0, "ryanvu2022@gmail.com")
-            password_entry.delete(0, END)
-            website_entry.focus()
+        with open("data.json", "r") as file:
+            # Reading old data
+            data = json.load(file)
+            # Updating old data with new data
+            data.update(new_data)
+        with open("data.json", "w") as file:
+            # Saving updated data
+            json.dump(data, file, indent=4)
+
+        website_entry.delete(0, END)
+        email_entry.delete(0, END)
+        email_entry.insert(0, "ryanvu2022@gmail.com")
+        password_entry.delete(0, END)
+        website_entry.focus()
 
 
 window = Tk()
@@ -65,9 +76,9 @@ password_label.config(pady=5)
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=45)
+website_entry = Entry(width=27)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 
 email_entry = Entry(width=45)
 email_entry.insert(0, "ryanvu2022@gmail.com")
